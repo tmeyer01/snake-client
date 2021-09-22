b6dc4b8e5e4e0637b6ff3626ee6383c528e68bf1
thomas <tmeyerwebdev@gmail.com>
2021-09-22 22:12:59 +0000

    Now using Stdin aka Standard In to dectect keyboard commands

--- play.js
+++ play.js
@@ -2,6 +2,25 @@ const {  connect } = require("./client");
 
 console.log("Connecting ...");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  console.log(key);
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+
+
 
 connect();
+setupInput();
 
