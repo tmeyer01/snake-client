eeafe96e3a099319ac445f60c73760c560e9882a
thomas <tmeyerwebdev@gmail.com>
2021-09-22 22:13:50 +0000

    removed spelling error

--- client.js
+++ client.js
@@ -8,12 +8,11 @@ const connect = function () {
     host: "135.23.223.133",
     port: "50542",
   });
-
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
   conn.on("data", (data)=>{
-    console.log("data > ", data);
+    console.log(data);
   })
   conn.on("connect", ()=>{
     console.log("Successfully connected to game server")
