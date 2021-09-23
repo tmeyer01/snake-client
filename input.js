9349e45c757a95342d655590ae83b06bca7764e3
thomas <tmeyerwebdev@gmail.com>
2021-09-23 01:15:52 +0000

    Pressing the Key P will now send a message that says KILr

--- input.js
+++ input.js
@@ -29,6 +29,9 @@ const handleUserInput = function(key) {
   if (key === "d"){
     connection.write("Move: right");
   }
+  if (key === "p"){
+    connection.write("Say: KILr");
+  }
   if (key === '\u0003') {
     process.exit();
   }
